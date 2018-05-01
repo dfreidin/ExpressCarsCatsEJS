51c6fcfb3119208695073bc1827744101ac2267b
dfreidin <36039896+dfreidin@users.noreply.github.com>
2018-05-01 15:38:51 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/static"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.get("/", function(request, response){
+    response.render("index");
+});
+app.get("/cars", function(request, response){
+    response.render("cars");
+});
+app.get("/cats", function(request, response){
+    response.render("cars");
+});
+app.get("/cars/new", function(request, response){
+    response.render("form");
+});
+app.listen(8000);
